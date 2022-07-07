7a94dd138ab57e64a82380571d76016c0c685472
halilfindik <halfin@gmail.com>
2022-07-07 18:17:41 +0300

    halil değişiklik

--- src/Deneme1.java
+++ src/Deneme1.java
@@ -2,5 +2,6 @@ public class Deneme1 {
     public static void main(String[] args) {
         System.out.println("deneme");
         System.out.println("Halil giriş yaptı");
+        System.out.println("Değişiklik yapıldı");
     }
 }
